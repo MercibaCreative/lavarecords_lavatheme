c400fa9b88da7a4253b9e49f6765b6f0d3aaa8ec
Alfonso Gober <alfonso@adfrogs.com>
2015-08-26 02:28:34 -0400

    Added templates for each header link

---
+++ page-events.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: Tours & Events Template
+*/
+?>
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+<div id="sb-site">
+<p>Tours & Events goes here</p>
---
+++ page-gallery.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: Gallery Template
+*/
+?>
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+<div id="sb-site">
+<p>Instagram stuff goes here</p>
