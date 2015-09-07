004ed8a55678c7f7203c5f9e900707bbfd4feb9a
Alfonso Gober <alfonso@adfrogs.com>
2015-09-06 21:16:16 -0400

    Added table structure for tours

--- content-artist.php
+++ content-artist.php
@@ -16,8 +16,6 @@ $tours = get_posts(array(
 <!-- ==========================CONTENT=========================== -->
 <section id="content" class="container">
     
-
-	
 	<section id="posts">
 
 		<div class="post">
@@ -26,40 +24,49 @@ $tours = get_posts(array(
                 <h1 class="text-center"><?php echo nl2p($post->post_title); ?></h1>
                 <div class="artist-links">
                 	<?php if ($artist["website_link"][0] != "") { ?>
-                		<a href="<?php echo $artist["website_link"][0]; ?>"><?php echo $artist["website_link"][0]; ?></a>
+                		<a href="http://<?php echo $artist["website_link"][0]; ?>"><?php echo $artist["website_link"][0]; ?></a>
                 	<?php } ?>
                 	<?php if ($artist["instagram_link"][0] != "") { ?>
-                		<a href="<?php echo $artist["instagram_link"][0]; ?>" target="_blank"><i class="fa fa-instagram"></i></a>
+                		<a href="https://instagram.com/<?php echo $artist["instagram_link"][0]; ?>" target="_blank"><i class="fa fa-instagram"></i></a>
                 	<?php } ?>
                 	<?php if ($artist["twitter_link"][0] != "") { ?>
-                		<a href="<?php echo $artist["twitter_link"][0]; ?>" target="_blank"><i class="fa fa-twitter"></i></a>
+                		<a href="https://twitter.com/<?php echo $artist["twitter_link"][0]; ?>" target="_blank"><i class="fa fa-twitter"></i></a>
 					<?php } ?>
 					<?php if ($artist["facebook_link"][0] != "") { ?>
-						<a href="<?php echo $artist["facebook_link"][0]; ?>" target="_blank"><i class="fa fa-facebook"></i></a>
+						<a href="https://facebook.com/<?php echo $artist["facebook_link"][0]; ?>" target="_blank"><i class="fa fa-facebook"></i></a>
                 	<?php } ?>
                 	<?php if ($artist["soundcloud_link"][0] != "") { ?>
-                		<a href="<?php echo $artist["soundcloud_link"][0]; ?>" target="_blank"><i class="fa fa-soundcloud"></i></a>
+                		<a href="https://soundcloud.com/<?php echo $artist["soundcloud_link"][0]; ?>" target="_blank"><i class="fa fa-soundcloud"></i></a>
                 	<?php } ?>
                 </div>
 				<?php echo nl2p($post->post_content); ?>
 			</div>
-            <h2 class="text-center">Current Tours</h2>
             
-			<ul class="tours">
-			<?php foreach($tours as $i => $tour): ?>
-				<?php $tour_meta = get_post_meta($tour->ID); ?>
-				<?php $tour_artists = $tour_meta["artists"][0]; ?>
-				<?php if (unserialize($tour_artists)[0] == $post->ID) { ?>
-					<li>
-						<?php echo($tour_meta["date"][0]); ?>, 
-						<?php echo($tour->post_title); ?>, 
-						<?php echo($tour_meta["type"][0]); ?>, 
-						<?php echo($tour_meta["venue"][0]); ?>, 
-						<?php echo($tour_meta["location"][0]); ?>
-					</li>
-				<?php } ?>
-			<?php endforeach; ?>
-			</ul>
+            <h2 class="text-center">Current Tours</h2>
+            <table class="tours">
+	            <thead>
+			      <tr>
+			        <th>Date</th>
+			        <th>Venue</th>
+			        <th>Location</th>
+			        <th></th>
+			      </tr>
+			    </thead>
+			    <tbody>
+			    	<?php foreach($tours as $i => $tour): ?>
+						<?php $tour_meta = get_post_meta($tour->ID); ?>
+						<?php $tour_artists = $tour_meta["artists"][0]; ?>
+						<?php if (unserialize($tour_artists)[0] == $post->ID) { ?>
+					      <tr>
+					        <td><?php echo($tour_meta["date"][0]); ?></td>
+					        <td><?php echo($tour_meta["venue"][0]); ?></td>
+					        <td><?php echo($tour_meta["location"][0]); ?></td>
+					        <td><a href="#">TICKETS</a></td>
+					      </tr>
+					    <?php } ?>
+					<?php endforeach; ?>
+			    </tbody>
+		  	</table>
 		
         </div>
 
