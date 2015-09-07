a59e09c4f05ee0bedff0186aae3e6c9e306b13c1
Alfonso Gober <alfonso@adfrogs.com>
2015-09-07 01:50:44 -0400

    news page

--- page-news.php
+++ page-news.php
@@ -2,7 +2,18 @@
 /*
 Template Name: News Template
 */
+
+$news = get_posts(array(
+		'post_type'        => 'post'
+		));
+
 ?>
 <?php get_header(); ?>
-<?php get_sidebar(); ?>
-<p>News goes here</p>
+<?php foreach($news as $i => $article): ?>
+	<?php $xmultiple = rand(1, 4); ?>
+	<?php $ymultiple = rand(1, 2); ?>
+	<div class="news-section col-md-<?php echo $xmultiple*6; ?>">
+		<img src="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $article->ID ), 'single-post-thumbnail' )[0] ?>" />
+		<h1><?php echo $article->post_title ?></h1>
+	</div>
+<?php endforeach; ?>
