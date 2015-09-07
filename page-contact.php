6759ac94432e24ba12769439ac4ab2554bb53a2b
Sofia Falcon <Sofia@Sofias-MacBook-Pro.local>
2015-09-07 00:37:07 -0400

    contact

--- page-contact.php
+++ page-contact.php
@@ -1,47 +1,61 @@
-<script>
-[{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#e9dcdc"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#f0e5e5"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ff6868"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.fill","stylers":[{"color":"#fa2525"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#d89d9d"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"color":"#f2eaea"}]},{"featureType":"poi.government","elementType":"geometry.fill","stylers":[{"color":"#e9dcdc"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"color":"#ff6868"}]},{"featureType":"transit.station","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"color":"#f2eaea"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"color":"#f2eaea"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"color":"#f2eaea"}]},{"featureType":"poi.attraction","elementType":"geometry","stylers":[{"color":"#f2eaea"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#d89d9d"}]},{"featureType":"landscape.natural","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"administrative.land_parcel","elementType":"geometry.fill","stylers":[{"color":"#eee9e9"}]},{"featureType":"poi.place_of_worship","elementType":"geometry","stylers":[{"color":"#f2eaea"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#e9dcdc"}]}]
-</script>
-
 <?php
 /*
 Template Name: Contact Template
 */
 ?>
 <?php get_header(); ?>
-<?php get_sidebar(); ?>
+
 
 <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $page->ID ), 'single-post-thumbnail' ); ?>
 
-<!-- ==========================MAIN CONTENT=========================== -->
 
-			<!-- ==========================CONTACT CONTENT=========================== -->
-			<section id="header" class="container-fluid" style="background-image: url('<?php echo $image[0];?>');">
-				<h1>
-					<?php
-						$post = get_post($page->ID); 
-						$title = apply_filters('the_title', $post->post_title); 
-						echo $title;
-					?>
-				</h1>
-				<div class="load"></div>
-			</section>
-			
+
+<section class="post-header container-fluid"> <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d6043.646794714543!2d-73.98694713998957!3d40.76590883358376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e1!4m0!4m5!1s0x89c258f7e8ed81c5%3A0x95557754cf1e8275!2s1755+Broadway+New+York%2C+NY+10019!3m2!1d40.7659008!2d-73.982323!5e0!3m2!1sen!2sca!4v1441599556646" width="100%" height="377" frameborder="0" style="border:0" allowfullscreen></iframe>
+</section>
+
+
+<!-- ==========================CONTENT=========================== -->
+<section id="content" class="container">
+    
+	<section id="posts">
+
+		<div class="post">
+
+			<div class="bodycopy">
+                <h1 class="text-center"><?php echo nl2p($post->post_title); ?></h1>
+                <img scr="img/logos/lava-main.png"/>
+				<h4 class="text-center"><?php echo nl2p($post->post_content); ?></h4>
+			</div>
+            
+
+            
+		
+        </div>
+
+	</section>
+
+</section>
+
+		
 			<!-- ==========================CONTENT=========================== -->
 			<section id="content" class="container">
 				<section id="contact" class="col-md-8">
 					<form id="contactform" role="form">
-						<div class="form-group">
+						
+                        <div class="form-group">
 							<div class="input-group">
 								<div class="input-group-addon"><i class="fa fa-user"></i></div>
 								<input id="contact-name" class="form-control" type="name" placeholder="Enter name">
 							</div>
 						</div>
+                        
 						<div class="form-group">
 							<div class="input-group">
 								<div class="input-group-addon">@</div>
 								<input class="form-control" id="contact-email" type="email" placeholder="Enter email">
 							</div>
 						</div>
+                        
 						<div class="form-group">
 							<select class="form-control" id="contact-subject">
 								<option>General</option>
@@ -50,42 +64,25 @@ Template Name: Contact Template
 								<option>Sales</option>
 							</select>
 						</div>
+                        
 						<div class="form-group">
 							<div class="input-group">
 								<div class="input-group-addon"><i class="fa fa-ellipsis-h"></i></div>
 								<textarea class="form-control" id="contact-message" type="message" placeholder="Enter message"></textarea>
 							</div>
 						</div>
+                        
 						<div class="form-group">
 							<button type="button" id="contact-send" class="btn btn-danger btn-lg btn-block">Send</button>
 						</div>
+                        
 					</form>
+                    
 				</section>
-				<div id="fb-root"></div>
-				<script>(function(d, s, id) {
-					var js, fjs = d.getElementsByTagName(s)[0];
-					if (d.getElementById(id)) return;
-					js = d.createElement(s); js.id = id;
-					js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
-					fjs.parentNode.insertBefore(js, fjs);
-				}(document, 'script', 'facebook-jssdk'));</script>
-				<section id="sidebar" class="col-md-4">
-					<h3>
-						Address
-					</h3>
-					<address>
-						<?php 
-							$post = get_post($page->ID); 
-							$content = apply_filters('the_content', $post->post_content); 
-							echo $content; 
-						?>
-					</address>
-				</section>
+				
 			</section>
 			
-			<section id="map" class="container">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.6175625839574!2d-73.98566399999997!3d40.748439999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire+State+Building!5e0!3m2!1sen!2sus!4v1405873188583" width="600" height="450" frameborder="0" style="border:0"></iframe>
-			</section>
+			
 
 			<style>
 				#FSContact1{
