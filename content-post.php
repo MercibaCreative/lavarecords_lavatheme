10774f9859139526c70252d55870ca4b17d75532
Alfonso Gober <alfonso@adfrogs.com>
2015-09-08 00:09:13 -0400

    news page done

--- content-post.php
+++ content-post.php
@@ -1,23 +1,27 @@
 <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 <!-- ==========================ABOUT CONTENT=========================== -->
-<section id="header" class="container-fluid" style="background-image: url('<?php echo $image[0];?>');">
-	<?php the_title( '<h1>', '</h1>' ); ?>
-	<div class="load"></div>
+<section class="post-header container-fluid"> <img src="<?php echo $image[0];?>" />
 </section>
 
 <!-- ==========================CONTENT=========================== -->
 <section id="content" class="container">
-	
-<section id="posts" class="col-md-12">
+    
+	<section id="posts">
 
-	<div class="post">
-		<a href="/news" class="back" style="font-size:16px; font-weight:bold; padding-bottom:10px; display:inline-block">&lt; Back</a>
-		<div class="info">
-			<i class="fa fa-calendar"></i> <?php echo date("F, j Y", strtotime($post->post_date));?>
+		<div class="post">
+
+			<div class="bodycopy">
+                <h1 class="text-center"><?php echo nl2p($post->post_title); ?></h1>
+		
+			<!--<a href="/news" class="back" style="font-size:16px; font-weight:bold; padding-bottom:10px; display:inline-block">&lt; Back</a>-->
+				<div class="artist-links text-center">
+					<?php echo date("F j, Y", strtotime($post->post_date));?> | <a href="#" id="share-toggle">Share</a>
+				</div>
+				<div class="content">
+					<?php echo nl2p($post->post_content); ?>
+				</div>
+			</div>
 		</div>
-		<div class="content">
-			<?php echo nl2p($post->post_content); ?>
-		</div>
-	</div>
+	</section>
 
 </section>
