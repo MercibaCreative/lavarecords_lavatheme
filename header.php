ccad5175f1ac569680738a83361b39be710e07f7
Sofia Falcon <Sofia@Sofias-MacBook-Pro.local>
2015-09-11 17:48:55 -0400

    favicon

--- header.php
+++ header.php
@@ -6,6 +6,10 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
+        <link rel="icon" href="/img/_favicon.ico" type="image/x-icon"/>
+        <link rel="shortcut icon" href="/img/_favicon.ico" type="image/x-icon"/>
+
+        
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
