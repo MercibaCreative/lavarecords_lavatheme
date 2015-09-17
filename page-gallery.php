acda7b4102ea1796db4808de3258703cbc53a0b4
Alfonso Gober <alfonso@adfrogs.com>
2015-09-17 01:55:06 -0400

    Instagram link

--- page-gallery.php
+++ page-gallery.php
@@ -4,6 +4,7 @@ Template Name: Gallery Template
 */
 ?>
 <?php get_header(); ?>
+<div class="instagram-page-link"><a href="https://instagram.com/lavarecordsofficial/"><i class="fa fa-instagram"></i>Follow Lava Records On Instagram</a></div>
 <script src="//instansive.com/widget/js/instansive.js"></script>
 <iframe src="//instansive.com/widgets/a61f167e313a05bbddcea8637f2f4dfa38b991fe.html" id="instansive_a61f167e31" name="instansive_a61f167e31"  scrolling="no" allowtransparency="true" class="instansive-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
 <?php get_footer(); ?>
