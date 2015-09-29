bad355c05d5f2531d51faef1db70f34a31c18195
Sofia Falcon <Sofia@Sofias-MacBook-Pro.local>
2015-09-28 23:44:54 -0400

    fav

--- header.php
+++ header.php
@@ -5,6 +5,9 @@
 		<title>Lava Records</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        
+        <link rel="icon" href="imgs/favicon.ico" type="image/x-icon"/>
+        <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon"/>
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
